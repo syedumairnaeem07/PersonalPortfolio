21a9d12a71d3747c2fffddbab9c3ccab229bcbea
umair <sumairn.1997@gmail.com>
2022-01-09 19:53:58 +0500

    Adds Header Section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,66 @@
-import Link from 'next/link';
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+import Link from "next/link";
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+import { DiCssdeck } from "react-icons/di";
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import {
+	Container,
+	Div1,
+	Div2,
+	Div3,
+	NavLink,
+	SocialIcons,
+	Span,
+	Logo,
+} from "./HeaderStyles";
 
-const Header = () =>  (
-  <div>
-    Header
-  </div>
+const Header = () => (
+	<Container>
+		<Div1>
+			<Link href="/">
+				<Logo>
+					<DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+				</Logo>
+			</Link>
+		</Div1>
+		<Div2>
+			<li>
+				<Link href="#projects">
+					<NavLink>Projects</NavLink>
+				</Link>
+			</li>
+			<li>
+				<Link href="#tech">
+					<NavLink>Technologies</NavLink>
+				</Link>
+			</li>
+			<li>
+				<Link href="#about">
+					<NavLink>About</NavLink>
+				</Link>
+			</li>
+		</Div2>
+		<Div3>
+			<SocialIcons
+				href="https://github.com/syedumairnaeem07"
+				target="_blank"
+			>
+				<AiFillGithub size="3rem" />
+			</SocialIcons>
+			<SocialIcons
+				href="https://www.linkedin.com/in/syedumairnaeem07/"
+				target="_blank"
+			>
+				<AiFillLinkedin size="3rem" />
+			</SocialIcons>
+			<SocialIcons
+				href="https://www.instagram.com/S_Umair07/"
+				target="_blank"
+			>
+				<AiFillInstagram size="3rem" />
+			</SocialIcons>
+		</Div3>
+	</Container>
 );
 
 export default Header;
