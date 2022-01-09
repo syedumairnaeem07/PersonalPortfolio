82531ea6875a2e310b2c49ba8eb29c70e1a9bd50
umair <sumairn.1997@gmail.com>
2022-01-09 20:21:27 +0500

    Adds Hero Section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+	Section,
+	SectionText,
+	SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+	<Section row nopadding>
+		<LeftSection>
+			<SectionTitle main center>
+				Greetings!
+				<br /> I'm Syed Umair Naeem
+			</SectionTitle>
+			<SectionText>
+				I am a passionate developer. My love for coding is what makes me
+				enjoy everything I do as a programmer. I find learning new
+				technologies and concepts pretty exciting. I feel comfortable
+				working either individually or with a team.
+			</SectionText>
+			<Button>Learn More</Button>
+		</LeftSection>
+	</Section>
 );
 
 export default Hero;
